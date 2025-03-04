7fa6dad428e915d99afec96b237bc9bee9fb5cc1
Jesús Miguel Lopez mesia <jesus_lopez_mm1995@outlook.com>
2025-03-04 03:49:15 +0000

    Updated Program.cs

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -22,6 +22,7 @@ using Microsoft.eShopWeb.Web.Pages;
 using Microsoft.FeatureManagement;
 using Microsoft.IdentityModel.Tokens;
 
+// Testing my PR
 // My first change
 // Testing my first PR
 var builder = WebApplication.CreateBuilder(args);
