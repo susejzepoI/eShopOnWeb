4ed17a0657172bea0c6cf9911d1bdb54d14c1bf2
Jesus Lopez Mesia <34073690+susejzepoI@users.noreply.github.com>
2025-05-10 18:42:45 -0500

    Update jlopez_azure_resource.ps1

--- infra/jlopez_azure_resource.ps1
+++ infra/jlopez_azure_resource.ps1
@@ -1,1 +1,29 @@
+[cmdletBinding()]
+param(
+    
+    [parameter(Mandatory = $true)]
+    [string]$RbacName,
 
+    [parameter(Mandatory = $true)]
+    [string]$RbacRole,
+
+    [Parameter(Mandatory = $true)]
+    [string]$ResourceGroupName,
+
+    [Parameter(Mandatory = $true)]
+    [string]$Location,
+
+    [Parameter(Mandatory = $true)]
+    [string]$SubscriptionId
+
+)
+
+az group create --name $ResourceGroupName --location $Location
+
+az configure --defaults group=$ResourceGroupName
+
+#JLopez-20250510: Create an application and its associated service principal, optionally configure the service principal's RBAC role assignments.
+$scope = "/subscriptions/$SubscriptionId/resourceGroups/$ResourceGroupName"
+az ad sp create-for-rbac --name $RbacName --role $RbacRole --scopes $scope --sdk-auth
+
+az provider register --namespace Microsoft.Web
